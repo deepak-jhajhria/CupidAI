d1c223e6ace441e044de4d800481107365c38afc
pooja-404 <pk3924792@gmail.com>
2024-04-06 14:25:47 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Faq from './components/Faq';
 import Ourservices from './components/Ourservices';
 import IdentityHub from './components/IdentityHub';
+import Pricing from './components/Pricing';
 
 function App() {
   return (
     <div className="bg-black">
       <Faq />
       <Ourservices />
+      <Pricing />
       <IdentityHub />
       {/* <Faq /> */}
     </div>
