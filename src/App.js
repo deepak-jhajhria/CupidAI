011ace9811b547b1d9d0db47577b8c726fd30b2b
Deepak jaat <deepakjhajhria4657@gmail.com>
2024-04-06 14:56:39 +0530

    update

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Faq from './components/Faq';
 import Footer from './components/Footer';
 import IdentityHub from './components/IdentityHub';
 import Crm from './components/Crm';
+import CustomizedAccordions from './components/FaqNew';
 
 function App() {
   return (
     <div className="bg-black">
+      <CustomizedAccordions />
       <Faq />
       <IdentityHub />
       <Footer />
