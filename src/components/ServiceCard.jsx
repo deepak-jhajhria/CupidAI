87d669f8604523ea03010504a8e718844f06a5c0
pooja-404 <pk3924792@gmail.com>
2024-04-07 11:34:03 +0530

    update

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -6,8 +6,8 @@ const ServiceCard = () => {
     return (
         <div className='py-32 bg-black bg-bg_gradient'>
             <div className='max-w-[966px] px-3  mx-auto '>
-                <h2 className='text-5xl font-bold text-center text-white font-inter md:leading-12'>Our Services</h2>
-                <p className=' font-medium text-dark-gray text-2xl text-center mx-auto leading-7 lg:max-w-[500px] pt-5'>Tweak anything and everything to ensure fits your business, not the other way around.</p>
+                <h2 className='text-4xl font-bold text-center text-white font-inter md:leading-12'>Our Services</h2>
+                <p className=' font-medium text-dark-gray text-xxl text-center mx-auto leading-7 lg:max-w-[500px] pt-5'>Tweak anything and everything to ensure fits your business, not the other way around.</p>
                 <div className='flex flex-row flex-wrap -mx-3 pt-7'>
                     {serviceCardItems.map((p) => <Ourservice key={p.id} svg={p.svges} head={p.heading} para={p.paragraph} width={p.width} />)}
                 </div>
