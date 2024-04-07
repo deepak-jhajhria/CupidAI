1b9fa92993899e9a20d21553dc69e40b2863215a
Deepak jaat <deepakjhajhria4657@gmail.com>
2024-04-07 09:26:44 +0530

    update

--- src/App.js
+++ src/App.js
@@ -2,29 +2,26 @@ import './App.css';
 import TrustedCompany from './components/TrustedCompany';
 import Faq from './components/Faq';
 import Crm from './components/Crm';
-import Ourservices from './components/Ourservices';
 import IdentityHub from './components/IdentityHub';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import ServiceCard from './components/ServiceCard';
 import Pricing from './components/Pricing';
-
+import dashboardimage from './assets/images/dashboardimage.webp'
 function App() {
   return (
     <div className=" bg-black-primary max-w-[1920px] mx-auto overflow-hidden">
-      <Header />      
+      <Header />
+      <img className='max-w-[915px] w-full mx-auto -translate-y-[40%]' src={dashboardimage} alt='image' />
       <TrustedCompany />
       <Crm />
       <div className='bg-[url(./assets/images/servicebg.webp)] bg-black bg-BgSize bg-top bg-no-repeat'>
         < ServiceCard />
       </div>
-      <Ourservices />
       <Pricing />
       <Faq />
       <IdentityHub />
       <Footer />
-      <Crm />
-      <TrustedCompany />
     </div>
   )
 
