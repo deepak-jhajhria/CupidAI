ebe9efff1569e7d4a1d821f6655bc1b1382b8e7e
khushi303 <maankhushi303@gmail.com>
2024-04-07 08:50:28 +0530

    change in responsive

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import IdentityHub from './components/IdentityHub';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import ServiceCard from './components/ServiceCard';
+import Pricing from './components/Pricing';
 
 function App() {
   return (
