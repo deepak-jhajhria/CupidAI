54a134fd36d69b7bfc811fae74462d9c05b47f82
khushi303 <maankhushi303@gmail.com>
2024-04-08 12:59:48 +0530

    change in hover

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
     const [isOpen, setOpen] = useOverflowHidden(false);
     return (
         <>
-            <div id="home" className="relative overflow-hidden min-h-[667px] md:min-h-[810px]">
+            <div className="relative overflow-hidden min-h-[667px] md:min-h-[810px]">
                 <span className="absolute bottom-0 left-0 z-0 w-full h-full"><Herobg /></span>
                 <div className="lg:max-w-[1320px] px-3 container mx-auto pt-4 sm:pt-5 md:pt-8 bg-[#01020f2e] relative">
                     <nav className="flex items-center justify-between rounded-full p-3 lg:p-2.5 border border-[#fbf0f040] border-solid">
@@ -38,7 +38,7 @@ const Header = () => {
                         </div>
                     </nav>
                 </div>
-                <div className="flex flex-col w-full justify-center items-center max-w-[691px] px-3 mx-auto relative z-10 mt-16 pt-1.5">
+                <div id="home" className="flex flex-col w-full justify-center items-center max-w-[691px] px-3 mx-auto relative z-10 mt-16 pt-1.5">
                     <h2 className="text-4xl font-semibold text-center text-white sm:text-5xl md:text-6xl lg:text-7xl md:leading-13">Unleash <span className="text-transparent bg-headerTextgradient bg-clip-text">Creativity</span>, Effortlessly</h2>
                     <p className="mt-3 text-base font-normal leading-8 text-center sm:text-lg md:text-xl text-gray-white-100 md:mt-4">Elevate your agency's output with state-of-the-art AI, engineered to innovate at the speed of thought.</p>
                     <CommonBtn cstm="mt-5" name="Try Now" />
