fa85e3cc05d35f798651fa22e8005d0ab53d1d88
pooja-404 <pk3924792@gmail.com>
2024-04-08 19:32:13 +0530

    width change

--- src/components/ServiceSec.jsx
+++ src/components/ServiceSec.jsx
@@ -5,7 +5,7 @@ import { serviceCardItems } from '../common/Helper'
 const ServiceCard = () => {
     return (
         <div className='lg:py-32 md:py-16 py-10 bg-black bg-bg_gradient'>
-            <div className='max-w-[990px] px-3   mx-auto '>
+            <div className='max-w-[966px] px-3   mx-auto '>
                 <h2 className='lg:text-4xl sm:text-3xl2 text-3xl1 font-bold text-center text-white font-inter md:leading-12'>Our Services</h2>
                 <p className=' font-medium text-gray sm:text-xxl text-xl  text-center mx-auto leading-7 lg:max-w-[500px] md:pt-3 pt-2 lg:pt-5'>Tweak anything and everything to ensure fits your business, not the other way around.</p>
                 <div className='flex flex-row flex-wrap -mx-3 sm:pt-5 pt-3 lg:pt-7'>
