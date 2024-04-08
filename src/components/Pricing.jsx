2659567ad92e0c4187472164c64b5762a7131b9d
Deepak jaat <deepakjhajhria4657@gmail.com>
2024-04-08 14:18:35 +0530

    update

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -280,31 +280,33 @@ const Pricing = () => {
                     <button onClick={() => setToggle(2)} className={`py-1.5 px-4.5 rounded-3md text-base font-medium text-white ${toggle === 2 ? "bg-gray-white-500" : ""}`}>Monthly</button>
                 </div>
                 <div className='flex items-center flex-wrap justify-center w-full md:pt-16 sm:pt-10 pt-6 lg:min-h-[826px]'>
-                    {toggle === 2 ? (pricedata.map((item, index) => {
-                        return (
-                            <div key={index} className='w-11/12 mt-8 lg:w-4/12 md:w-6/12 sm:w-10/12 lg:mt-0 md:mt-10'>
-                                <div className='relative transition-all duration-500 ease-linear cursor-pointer blur_box_border hover:scale-[1.02] hover:z-10 group '>
-                                    <div className='p-8 bg-gray-white-600 rounded-3xl'>
-                                        <h3 className='inline-block text-lg font-medium text-white transition-all duration-500 ease-linear group-hover:text-3xl1 leading-3xl group-hover:bg-clip-text group-hover:bg-textgradient group-hover:text-transparent'>{item.heading}</h3>
-                                        <p className='my-6 text-base font-medium leading-xl text-dark-gray'>{item.para}</p>
-                                        <div className='flex items-center gap-2'>
-                                            <h3 className='font-semibold text-white sm:text-4xl2 text-3xl2 lg:text-5xl leading-sm'>{item.price}</h3>
-                                            <div className='flex flex-col h-full'>
-                                                <p className='font-medium text-2sm text-dark-gray leading-2xl'>/ month</p>
-                                                <p className='font-medium text-2sm text-dark-gray leading-2xl'>{item.user}</p>
+                    {toggle === 2 && <>
+                        {
+                            pricedata.map((item, index) => (
+                                <div key={index} className='w-11/12 mt-8 lg:w-4/12 md:w-6/12 sm:w-10/12 lg:mt-0 md:mt-10'>
+                                    <div className='relative transition-all duration-500 ease-linear cursor-pointer blur_box_border hover:scale-[1.02] hover:z-10 group '>
+                                        <div className='p-8 bg-gray-white-600 rounded-3xl'>
+                                            <h3 className='inline-block text-lg font-medium text-white transition-all duration-500 ease-linear group-hover:text-3xl1 leading-3xl group-hover:bg-clip-text group-hover:bg-textgradient group-hover:text-transparent'>{item.heading}</h3>
+                                            <p className='my-6 text-base font-medium leading-xl text-dark-gray'>{item.para}</p>
+                                            <div className='flex items-center gap-2'>
+                                                <h3 className='font-semibold text-white sm:text-4xl2 text-3xl2 lg:text-5xl leading-sm'>{item.price}</h3>
+                                                <div className='flex flex-col h-full'>
+                                                    <p className='font-medium text-2sm text-dark-gray leading-2xl'>/ month</p>
+                                                    <p className='font-medium text-2sm text-dark-gray leading-2xl'>{item.user}</p>
+                                                </div>
+                                                {item.disbtn}
                                             </div>
-                                            {item.disbtn}
+                                            {item.billed}
+                                            <p className='mb-4 font-medium text-2sm text-dark-gray leading-2xl'>What’s included</p>
+                                            {item.includedata}
+                                            <button className='py-2.5 px-3 border border-solid border-gray-black-200 group-hover:bg-btngradient group-hover:border-transparent transition-all duration-500 ease-linear text-white text-base font-medium text-nowrap rounded-xl w-full'>Select Plan</button>
                                         </div>
-                                        {item.billed}
-                                        <p className='mb-4 font-medium text-2sm text-dark-gray leading-2xl'>What’s included</p>
-                                        {item.includedata}
-                                        <button className='py-2.5 px-3 border border-solid border-gray-black-200 group-hover:bg-btngradient group-hover:border-transparent transition-all duration-500 ease-linear text-white text-base font-medium text-nowrap rounded-xl w-full'>Select Plan</button>
                                     </div>
-                                </div>
-                            </div >
-                        )
-                    })) : ""}
-                    {toggle === 1 ? (pricedata1.map((item, index) => {
+                                </div >
+                            ))
+                        }
+                    </>}
+                    {toggle === 1 && (pricedata1.map((item, index) => {
                         return (
                             <>
                                 <div key={index} className='w-11/12 mt-8 lg:w-4/12 md:w-6/12 sm:w-10/12 lg:mt-0 md:mt-10'>
@@ -329,7 +331,7 @@ const Pricing = () => {
                                 </div >
                             </>
                         )
-                    })) : ""}
+                    }))}
                 </div>
                 <div className='flex items-center justify-center'>
                     <a href='#' className='bg-gray-white-500 inline-flex items-center justify-center sm:flex-row flex-col text-white px-4 py-1.5 sm:rounded-2md rounded-3xl sm:gap-2 gap-1 mt-12'><p className='font-medium text-2sm leading-2xl opacity-60 tracking-tightest'>Want enterprise features?</p><p className='flex items-center gap-1 font-bold text-2sm tracking-tightest leading-2xl'>Tell us more <WhiteArrow /></p></a>
