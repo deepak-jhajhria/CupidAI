517fa93a43685ffb7ff77b4d044798ab3cab10ca
Deepak jaat <deepakjhajhria4657@gmail.com>
2024-04-08 09:38:55 +0530

    update

--- src/App.js
+++ src/App.js
@@ -7,12 +7,10 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import ServiceCard from './components/ServiceCard';
 import Pricing from './components/Pricing';
-import dashboardimage from './assets/images/dashboardimage.webp'
 function App() {
   return (
     <div className=" bg-black-primary max-w-[1920px] mx-auto overflow-hidden">
       <Header />
-      <img className='max-w-[915px] w-full mx-auto -translate-y-[40%]' src={dashboardimage} alt='image' />
       <TrustedCompany />
       <Crm />
       <div className='bg-[url(./assets/images/servicebg.webp)] bg-black bg-BgSize bg-top bg-no-repeat'>
